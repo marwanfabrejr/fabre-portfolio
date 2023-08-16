963df7dff73a5c6681fcb871e8365bdf8060f886
Marwan Zayed <marwanzayed4444@gmail.com>
2023-08-16 14:50:41 +0300

    car showcase added

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -27,7 +27,6 @@ const Portfolio = () => {
               <div className="content">
                 <p className="title">{port.title}</p>
                 <h4 className="technologies">{port.technologies}</h4>
-                <p className="description">{port.description}</p>
                 <button className="btn" onClick={() => window.open(port.url)}>
                   View
                 </button>
